59b469abaa284ada23151ca65eefb2f3cfdff6f8
vmarorti <marsmotionless98@gmail.com>
2024-10-11 22:09:48 -0500

    Added code to models

---
+++ models/blog.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create Blog model and datatypes, including the user_id foreign key.
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;
---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Project = require('./Blog');
+const Blog = require('./Blog');
+
+// Creates a relationship between User and Project model, with the User having a "has many" relationship with Project model.
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// Creates a relationship between User and Project model, with a "belongs to" relationship of the Project to the User.
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
